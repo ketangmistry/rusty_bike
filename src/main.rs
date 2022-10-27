db03aa81d5456faccc988613e78ef91ca1e8b9fe
Ketan Mistry <ketan.g.mistry@gmail.com>
2022-10-27 16:29:30 +0100

    added dependencies for html builder.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::{FileServer, relative};
+use rocket::response::content::RawHtml;
+
 mod data;
 
 use data::bikes;
 
-fn main() {
+#[get["/"]]
+fn index() -> RawHtml<String> {
     let bike_list = bikes::get_bikes();
-    println!(
-        "There are {} bikes in the data file.",
-        bike_list.bikes.len()
-    );
+    let mut response = String::new();
+    response.push_str("There are ");
+    response.push_str(&bike_list.bikes.len().to_string());
+    response.push_str(" bikes in the data file.");
+    RawHtml(response)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/favicon.ico", FileServer::from(relative!("./src/static")))
 }
