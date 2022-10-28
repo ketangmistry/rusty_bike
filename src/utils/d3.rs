8135cf38a439ece00b98abd03d6e7e9e46503685
Ketan Mistry <ketan.g.mistry@gmail.com>
2022-10-28 15:47:24 +0100

    added a struct impl for a basic test.

--- src/utils/d3.rs
+++ src/utils/d3.rs
@@ -1,8 +1,16 @@
-use crate::bikes::Bikes;
+use crate::bikes::*;
 
+#[derive(Default)]
 struct Parent {
     name: String,
     children: Vec<Children>
+}
+
+impl Parent {
+    fn set_empty_defaults(&mut self, name: String) {
+        self.name = name;
+        self.children = Vec::new();
+    }
 }
 
 struct Children {
@@ -15,10 +23,56 @@ struct Child {
     size: i16
 }
 
-fn get_json_for_tree(bike_list: Bikes) {
-    let mut out = String::new();
-    out.push_str("There are ");
-    out.push_str(&bike_list.bikes.len().to_string());
-    out.push_str(" bikes in the data file.");
-    println!("{}",out);
+fn get_json_for_tree(_bike_list: Bikes) -> Parent {
+    let mut parent = Parent {..Default::default()};
+    parent.set_empty_defaults(String::from("rusty_bikes"));
+
+    // if bike_list.bikes.len() > 0 {
+    //     for item in bike_list.bikes {
+            
+    //         let mut child = Child {
+    //             name: item.bike.manufacturer,
+    //             size: 1234
+    //         };
+
+    //         parent.children.push(child)
+    //     }
+    // }
+
+    parent
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_json_for_tree() {
+
+        // bulid test data structure
+        let problem = Problem {
+            component: String::from("component"),
+            description: String::from("description"),
+            resolution: String::from("resolution")
+        };
+
+        let bike_data = BikeData {
+            manufacturer: String::from("manufacturer"),
+            model: String::from("model"),
+            year: 2022,
+            month: 01,
+            problems: vec![problem]
+        };
+
+        let bike = Bike {
+            bike: bike_data
+        };
+
+        let bikes_list = Bikes {
+            bikes: vec![bike]
+        };
+
+        let parent = get_json_for_tree(bikes_list);
+        assert!(parent.name == "rusty_bikes");
+    }
 }
