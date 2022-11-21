81f7cec18c81ebb57a963fb422d8ef9dd25d6467
Ketan Mistry <ketan.g.mistry@gmail.com>
2022-11-21 12:27:06 +0000

    added log4rs libs and incorporated traceID in log file.

---
+++ src/internals/tracing.rs
@@ -0,0 +1,17 @@
+use opentelemetry::sdk::Resource;
+use opentelemetry::{
+    sdk::trace as sdktrace, trace::TraceError
+};
+
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_semantic_conventions as semcov;
+
+pub fn init_tracer() -> Result<sdktrace::Tracer, TraceError> {
+    let resource = Resource::new(vec![semcov::resource::SERVICE_NAME.string("rusty_bike")]);
+
+    opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(opentelemetry_otlp::new_exporter().tonic().with_env())
+        .with_trace_config(sdktrace::config().with_resource(resource))
+        .install_simple()
+}
